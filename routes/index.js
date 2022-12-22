772ff419e2bdb987fa9765d657b35574d254aa12
dotdioscorea <github@awr.sh>
2022-12-22 21:04:24 +0000

    Added page numbers to homepage

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,47 @@ const User = require("../models/userSchema");
 const jwt = require("jsonwebtoken");
 const moment = require('moment')
 
-router.get("/", async (req, res) => {
-  Server.find({}).sort({ upvotes: -1 }).limit(10).exec(async (err, servers) => {
+
+// router.get("/", async (req, res) => {
+//   Server.find({}).sort({ upvotes: -1 }).limit(10).exec(async (err, servers) => {
+//     if (err) {
+//       return res.status(500).send(err);
+//     }
+//     for (let i = 0; i < servers.length; i++) {
+//       try {
+//         user = await User.findById(servers[i].ownerID);
+//         servers[i].owner = user.username;
+//       }
+//       catch (error) {}
+//     }
+
+//     res.render("home", { servers, moment });
+//   });
+// });
+
+router.get('/', function(req, res, next) {
+  // Calculate the page number and number of results per page
+  const page = req.query.page || 1;
+  const resultsPerPage = 2;
+
+  // Find the total number of results in the database
+  Server.countDocuments({}, function(err, count) {
     if (err) {
-      return res.status(500).send(err);
-    }
-    for (let i = 0; i < servers.length; i++) {
-      try {
-        user = await User.findById(servers[i].ownerID);
-        servers[i].owner = user.username;
-      }
-      catch (error) {}
+      return next(err);
     }
 
-    res.render("home", { servers, moment });
+    // Find the results for the current page
+    Server.find({})
+      .skip((page - 1) * resultsPerPage)
+      .limit(resultsPerPage)
+      .exec(function(err, servers) {
+        if (err) {
+          return next(err);
+        }
+        count = 50;
+        // Render the homepage template with the results
+        res.render('home', { servers: servers, page: page, totalPages: Math.ceil(count / resultsPerPage) });
+      });
   });
 });
 
